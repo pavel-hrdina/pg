e59aa74e0d752c2b5efcf4fe7ea6894d6a382955
Pavel Hrdina <pavel.hrdina.ml@seznam.cz>
2024-10-03 13:43:09 +0200

    Add sudy_nebo_lich function in first.py

---
+++ first.py
@@ -0,0 +1,9 @@
+def sudy_nebo_lich(cislo: int):
+    if cislo % 2 == 0:
+        print(f"{cislo} je sudé.")
+    else:
+        print(f"{cislo} je liché.")
+
+
+sudy_nebo_lich(1000)
+sudy_nebo_lich(5)
