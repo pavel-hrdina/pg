e6d47694240a0070e4f0fb0279bf2e1bd781836f
Pavel Hrdina <pavel.hrdina.ml@seznam.cz>
2024-11-14 10:39:40 +0100

    fix path to requirements.txt in ci workflows

--- sixth.py
+++ sixth.py
@@ -1,6 +1,7 @@
 import sys
 
 import requests
+from bs4 import BeautifulSoup
 
 
 def download_url_and_get_all_hrefs(url):
@@ -10,26 +11,27 @@ def download_url_and_get_all_hrefs(url):
     pokud ano, najdete ve stazenem obsahu stranky response.content vsechny vyskyty
     <a href="url">odkaz</a> a z nich nactete url, ktere vratite jako seznam pomoci return
     """
-    response = requests.get(url)
-    if response.status_code == 200:
-        odkazy = response.content.decode("utf-8")
-        hrefs = []
-        # Ziskani vsech odkazu
-        while odkazy.find("<a href=") != -1:
-            start_index = odkazy.find("<a href=")
-            end_index = odkazy.find(">", start_index)
-            href = odkazy[start_index + 9:end_index - 1]
-            hrefs.append(href)
-            odkazy = odkazy[end_index + 1:]
-        return hrefs
-    else:
-        print("Chyba pri stahovani stranky")
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"Chyba pri stahovani stranky: {e}")
         return []
+
+    if response.status_code == 200:
+        # Use BeautifulSoup to parse the HTML content
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        hrefs = [a['href'] for a in soup.find_all('a', href=True)]
+
+        return hrefs
+
+    return []
 
 
 if __name__ == "__main__":
     try:
         url = sys.argv[1]
-        download_url_and_get_all_hrefs(url)
+        hrefs = download_url_and_get_all_hrefs(url)
     except Exception as e:
         print(f"Program skoncil chybou: {e}")
